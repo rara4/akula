21cff9bdc16a684bf4881dc6baad10bf9600d755
rara4 <aghoutaneb@gmail.com>
2021-04-08 17:16:19 +0200

    🦈🦈 enhanced

--- src/extension.ts
+++ src/extension.ts
@@ -3,31 +3,21 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
     var rab = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -100);
-    rab.text = "Le gang des requins";
-    rab.tooltip = "requin gang faites attention";
-    rab.command = 'akula.funone';
+    rab.text = "Le gang des requins"; rab.tooltip = "requin gang faites attention"; rab.command = 'akula.funone';
     rab.show();
+
     let dispo = vscode.commands.registerCommand('akula.funone', () => {
         rab.text = `$(sync~spin)`;
-        const apr = setInterval(() => {
+        const apr = setTimeout(() => {
             rab.text = "🦈🦈🦈🦈🦈🦈";
-        }, 3000);
-        const av = setInterval(() => {
+            vscode.window.showInformationMessage("You are now a requin 🦈🦈");
+        }, 1500);
+        const apv = setTimeout(() => {
             rab.text = "Le gang des requins";
-        }, 5000)
+        }, 3000);
     }); 
-    
-	/* The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('akula.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from akula!');
-	}); 
-
-	context.subscriptions.push(disposable); */
+	context.subscriptions.push(dispo);
 }
 
 
